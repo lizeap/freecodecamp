66bdb485b7c26a0bb23d71ea3e71c6daeb920122
Lizeap <elizabethpasternak@gmail.com>
2018-11-27 20:03:12 -0500

    added reversestring

---
+++ reversestring.js
@@ -0,0 +1,24 @@
+function reverseString(str) {
+    var splitStr = str.split("");
+    //console.log(splitStr);
+    var reversedStr = splitStr.reverse();
+    //console.log(reversedStr);
+    var joinStr = reversedStr.join("");
+    //console.log(joinStr);
+   return joinStr;
+   
+   
+     return str;
+   }
+   
+   
+   reverseString("hello");
+   
+   /*pseudocode
+   space out word into individual letters
+   reverse the letters
+   put word back together */
+   
+   
+   
+   
