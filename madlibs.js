518bcd4ddc4090548fe2200cee5e0a8fa8068304
Lizeap <elizabethpasternak@gmail.com>
2018-11-27 20:00:50 -0500

    completed madlibs

--- madlibs.js
+++ madlibs.js
@@ -1,1 +1,10 @@
-wordBlanks("cat", "little", "hit", "slowly");
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    // Your code below this line
+    var result = "The scrappy "  + myNoun + " ate a very " + myAdjective + " tomato and " + myVerb + " " + myAdverb + " away.";
+    
+    // Your code above this line
+    return result;
+  }
+  
+  // Change the words here to test your function
+  wordBlanks("cat", "little", "hit", "slowly");
